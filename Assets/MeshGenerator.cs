ed3401150ac93a728bb4f0b5155c7ab35d1b5acd
Warren <warrenheartmanagement@gmail.com>
2020-05-18 12:16:11 -0700

    Playing with moving Meshes
    I went in and modified the Mesh code to see if we can move it in the update.

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -8,7 +8,8 @@ public class MeshGenerator : MonoBehaviour{
 
     Vector3[] vertices;
     int[] triangles;
-    
+
+    int i = 1;
     
     void Start(){
         mesh = new Mesh();
@@ -17,13 +18,20 @@ public class MeshGenerator : MonoBehaviour{
         CreateShape();
         UpdateMesh();
     }
+    void Update()
+    {
+        i -= 1;
+        CreateShape();
+        UpdateMesh();
+    }
 
+    // Toying around with Update modifying the mesh using an i to gradually increase the shape.
     // Update is called once per frame
     void CreateShape(){
         vertices = new Vector3[]{
             new Vector3 (0,0,0),
-            new Vector3 (0,0,20),
-            new Vector3 (20,0,0)
+            new Vector3 (0,0,-i),
+            new Vector3 (-i,0,0)
         };
 
         triangles = new int[]{
